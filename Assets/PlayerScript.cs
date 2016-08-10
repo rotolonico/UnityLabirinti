b52bdf18b22a5ea309c268a40347190066d61a34
domenico <rotolonico@gmail.com>
2016-08-10 18:42:25 +0200

    first commit

---
+++ Assets/PlayerScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+	private AudioSource n;
+
+	void OnTriggerEnter2D(Collider2D col) {
+		string colliderTag = col.gameObject.tag;
+		string colliderName = col.gameObject.name;
+
+		if (colliderTag == "wall") {
+
+			n.Play ();
+			print ("Musica!");
+
+
+		} else if (colliderName == "end") {
+			Application.LoadLevel ("1");
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		n = GetComponent<AudioSource>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (Input.GetAxis ("Horizontal") * Time.deltaTime * 4f, 0f, 0f, Space.World);
+		transform.Translate (0f, Input.GetAxis ("Vertical") * Time.deltaTime * 4f, 0f, Space.World);
+	}
+}
---
+++ Assets/WallScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WallScript : MonoBehaviour {
+	AudioSource ErrorSound;
+
+
+	void OnTriggerEnter2D(Collider2D col) {
+		string colliderName = col.gameObject.name;
+
+		if (colliderName == "player") {
+			
+
+			ErrorSound.Play ();
+
+		} else if (colliderName == "end") {
+			Application.LoadLevel ("1");
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		ErrorSound = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
